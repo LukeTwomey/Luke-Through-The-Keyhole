24714db954f390b8d4b7f5e7552dcf169b52f2da
Luke Twomey <luke.twomey85@gmail.com>
2016-04-14 19:16:34 +0100

    Add additional articles to homepage. All temporary (static) until database has been built.

--- index.php
+++ index.php
@@ -12,6 +12,33 @@
                 <!-- Add page content here -->
                 <div class="page-content index">
                     <article>
+                        <img class="featured-image" src="/images/hong-kong-explore.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Exploring Hong Kong</h2>
+                            <p>CouchSurfing. Hands up if you've heard of it! No prizes if I...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+
+                    <article>
+                        <img class="featured-image" src="/images/hong-kong.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Arrival in Hong Kong</h2>
+                            <p>Well, I made it! The flight wasn't the most fun in the world, I don't...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+
+                    <article>
+                        <img class="featured-image" src="/images/itinerary.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">The Itinerary</h2>
+                            <p>Other than the order of the places I'm visiting, I'm trying to keep th...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+
+                    <article>
                         <img class="featured-image" src="/images/welcome.jpg"/>
                         <div class="preview-text">
                             <h2 class="post-title">Hello and Welcome!</h2>
