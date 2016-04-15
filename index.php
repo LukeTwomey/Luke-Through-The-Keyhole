a9f2c966c752f6a3a1bf88c076c7a10a4ec2f7d6
Luke Twomey <luke.twomey85@gmail.com>
2016-04-15 15:36:35 +0100

    Add more posts to homepage.

--- index.php
+++ index.php
@@ -12,6 +12,71 @@
                 <!-- Add page content here -->
                 <div class="page-content index">
                     <article>
+                        <img class="featured-image" src="/images/ho-chi-minh.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Ho Chi Minh City</h2>
+                            <p>After the journey I'd been on to get here, Ho Chi Minh City certainly...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+                    <article>
+                        <img class="featured-image" src="/images/hanoi-saigon-2.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Hanoi to Saigon - Part Two</h2>
+                            <p>Day five should have seen us arrive in Khe Sanh after a two hundred ki...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+                    <article>
+                        <img class="featured-image" src="/images/hanoi-saigon.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Hanoi to Saigon - Part One</h2>
+                            <p>This was it, the big one. The Northwest Loop was just the practice run...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+                    <article>
+                        <img class="featured-image" src="/images/vietnam-by-bike.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Vietnam by Motorbike</h2>
+                            <p>So I bought a motorbike. A "Honda Win" that isn't actually a Honda, bu...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+                    <article>
+                        <img class="featured-image" src="/images/hanoi.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Arrival in Hanoi</h2>
+                            <p>They played surreal, remixed, poor quality Vietnamese music videos on...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+                    <article>
+                        <img class="featured-image" src="/images/nanning.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Arrival in Nanning</h2>
+                            <p>The first train journey on my trip so far was mercifully uneventful. I...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+                    <article>
+                        <img class="featured-image" src="/images/guilin.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Arrival in Guilin</h2>
+                            <p>The drivers here are INSANE. I'd only been in Guilin half an hour and...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+                    <article>
+                        <img class="featured-image" src="/images/macau.jpg"/>
+                        <div class="preview-text">
+                            <h2 class="post-title">Macau and Farewell to Hong Kong</h2>
+                            <p>I'd read that a trip to Hong Kong wasn't complete without a visit to M...</p>
+                            <a href="#" class="button">Read more...</a>
+                        </div>
+                    </article>
+
+                    <article>
                         <img class="featured-image" src="/images/hong-kong-explore.jpg"/>
                         <div class="preview-text">
                             <h2 class="post-title">Exploring Hong Kong</h2>
