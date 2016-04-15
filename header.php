a61bbe6a799f6d580fa51055b6dc0a42c9fa0def
Luke Twomey <luke.twomey85@gmail.com>
2016-04-15 17:51:32 +0100

    Add max width to header.

--- header.php
+++ header.php
@@ -1,11 +1,13 @@
 <header>
-    <img class="menu" src="/images/icons/menu.png"/>
-    <div class="logo-nav-bar">
-        <a href="index.php">
-            <img class="logo" src="/images/logo.png"/>
-        </a>
-        <nav>
-            <?php include("nav.php"); ?>
-        </nav>
+    <div class="content">
+        <img class="menu" src="/images/icons/menu.png"/>
+        <div class="logo-nav-bar">
+            <a href="index.php">
+                <img class="logo" src="/images/logo.png"/>
+            </a>
+            <nav>
+                <?php include("nav.php"); ?>
+            </nav>
+        </div>
     </div>
 </header>
