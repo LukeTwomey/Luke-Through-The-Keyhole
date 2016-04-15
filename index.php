e7190c6204106ef131edc58bcbf0c2bcb68be0fe
Luke Twomey <luke.twomey85@gmail.com>
2016-04-15 16:29:00 +0100

    Add hyperlinks to featured images and post titles.

--- index.php
+++ index.php
@@ -12,101 +12,101 @@
                 <!-- Add page content here -->
                 <div class="page-content index">
                     <article>
-                        <img class="featured-image" src="/images/ho-chi-minh.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/ho-chi-minh.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Ho Chi Minh City</h2>
+                            <a href="#"><h2 class="post-title">Ho Chi Minh City</h2></a>
                             <p>After the journey I'd been on to get here, Ho Chi Minh City certainly...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
                     <article>
-                        <img class="featured-image" src="/images/hanoi-saigon-2.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/hanoi-saigon-2.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Hanoi to Saigon - Part Two</h2>
+                            <a href="#"><h2 class="post-title">Hanoi to Saigon - Part Two</h2></a>
                             <p>Day five should have seen us arrive in Khe Sanh after a two hundred ki...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
                     <article>
-                        <img class="featured-image" src="/images/hanoi-saigon.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/hanoi-saigon.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Hanoi to Saigon - Part One</h2>
+                            <a href="#"><h2 class="post-title">Hanoi to Saigon - Part One</h2></a>
                             <p>This was it, the big one. The Northwest Loop was just the practice run...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
                     <article>
-                        <img class="featured-image" src="/images/vietnam-by-bike.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/vietnam-by-bike.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Vietnam by Motorbike</h2>
+                            <a href="#"><h2 class="post-title">Vietnam by Motorbike</h2></a>
                             <p>So I bought a motorbike. A "Honda Win" that isn't actually a Honda, bu...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
                     <article>
-                        <img class="featured-image" src="/images/hanoi.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/hanoi.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Arrival in Hanoi</h2>
+                            <a href="#"><h2 class="post-title">Arrival in Hanoi</h2></a>
                             <p>They played surreal, remixed, poor quality Vietnamese music videos on...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
                     <article>
-                        <img class="featured-image" src="/images/nanning.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/nanning.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Arrival in Nanning</h2>
+                            <a href="#"><h2 class="post-title">Arrival in Nanning</h2></a>
                             <p>The first train journey on my trip so far was mercifully uneventful. I...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
                     <article>
-                        <img class="featured-image" src="/images/guilin.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/guilin.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Arrival in Guilin</h2>
+                            <a href="#"><h2 class="post-title">Arrival in Guilin</h2></a>
                             <p>The drivers here are INSANE. I'd only been in Guilin half an hour and...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
                     <article>
-                        <img class="featured-image" src="/images/macau.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/macau.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Macau and Farewell to Hong Kong</h2>
+                            <a href="#"><h2 class="post-title">Macau and Farewell to Hong Kong</h2></a>
                             <p>I'd read that a trip to Hong Kong wasn't complete without a visit to M...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
 
                     <article>
-                        <img class="featured-image" src="/images/hong-kong-explore.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/hong-kong-explore.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Exploring Hong Kong</h2>
+                            <a href="#"><h2 class="post-title">Exploring Hong Kong</h2></a>
                             <p>CouchSurfing. Hands up if you've heard of it! No prizes if I...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
 
                     <article>
-                        <img class="featured-image" src="/images/hong-kong.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/hong-kong.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Arrival in Hong Kong</h2>
+                            <a href="#"><h2 class="post-title">Arrival in Hong Kong</h2></a>
                             <p>Well, I made it! The flight wasn't the most fun in the world, I don't...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
 
                     <article>
-                        <img class="featured-image" src="/images/itinerary.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/itinerary.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">The Itinerary</h2>
+                            <a href="#"><h2 class="post-title">The Itinerary</h2></a>
                             <p>Other than the order of the places I'm visiting, I'm trying to keep th...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
                     </article>
 
                     <article>
-                        <img class="featured-image" src="/images/welcome.jpg"/>
+                        <a href="#"><img class="featured-image" src="/images/welcome.jpg"/></a>
                         <div class="preview-text">
-                            <h2 class="post-title">Hello and Welcome!</h2>
+                            <a href="#"><h2 class="post-title">Hello and Welcome!</h2></a>
                             <p>My name's Luke Twomey, and if you're reading this I've probably harass...</p>
                             <a href="#" class="button">Read more...</a>
                         </div>
