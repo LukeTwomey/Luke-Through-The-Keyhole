553c64068647989bb8b25c550537b3eeb7a236a5
Luke Twomey <luke.twomey85@gmail.com>
2016-04-16 15:20:59 +0100

    Add footer.

--- footer.php
+++ footer.php
@@ -1,2 +1,9 @@
 <footer>
+    <div class="social">
+        <a href="http://www.facebook.com"><img src="images/icons/facebook.png"/></a>
+        <a href="http://www.twitter.com"><img src="images/icons/twitter.png"/></a>
+    </div>
+    <div class="copyright">
+        <p>Copyright &copy; Luke Twomey 2016</p>
+    </div>
 </footer>
