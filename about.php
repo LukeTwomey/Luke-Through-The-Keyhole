b4e55684ca8b0ec6db02a8ace25f1ea9b39d74f0
Luke Twomey <luke.twomey85@gmail.com>
2016-04-19 19:37:04 +0100

    Create about page.

---
+++ about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <!-- Add page content here -->
+                <div class="page-content about">
+                    <p>Hi, I'm Luke.</p>
+                    <p>My surname is Twomey (Irish, and notoriously hard to pronounce for the uninitiated). It's too-me by the way.</p>
+                    <p>I've been working as a technical consultant in a software company for the past five years. This fully qualifies me to dispense the following invaluable advice regarding the problem you're no doubt having with your computer; turn it off and on again.</p>
+                    <p>Five years in a job is a relatively long time. I felt like I needed a change of direction. An opportunity arose to travel to Sydney to visit some friends. At its inception I intended to spend one month there. This evolved into spending a year travelling around Australia. This further evolved into spending a year backpacking Southeast Asia, Australia, New Zealand and New York. It hasn't yet got to the stage where I find myself trekking to the North Pole, but I'm sure it's only a matter of time.</p>
+                    <p>I set up this blog for my own personal enjoyment, and sharing my experiences with friends and family.</p>
+                    <p>Join me as I journey across the globe, experiencing soaring highs and crushing lows (hopefully less of the latter).</p>
+                    <p>Check out my Flickr photostream which will be regularly updated with the amazing moments I capture along the way.</p>
+                </div>
+
+                <img class="back-to-top hidden" src="/images/icons/up-arrow.jpg"/>
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
