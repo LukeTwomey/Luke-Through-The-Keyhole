4b6981d67c16a38cecab4af7ebb8169ad3982e3c
Luke Twomey <luke.twomey85@gmail.com>
2016-04-25 20:25:35 +0100

    Create shells of static blog post pages. DB functionality to be added at a later date.

---
+++ blog/arrival-in-hanoi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include($_SERVER["DOCUMENT_ROOT"] . "/header.php"); ?>
+
+                <!-- Add page content here -->
+                <div class="page-content arrival-in-hanoi">
+
+                </div>
+
+                <img class="back-to-top hidden" src="/images/icons/up-arrow.jpg"/>
+                <?php include($_SERVER["DOCUMENT_ROOT"] . "/footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include($_SERVER["DOCUMENT_ROOT"] . "/javascript-files.php"); ?>
+    </body>
+</html>
