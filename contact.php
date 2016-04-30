c84515c420b2fca4a6a218ee3c914a71d0c5ad1c
Luke Twomey <luke.twomey85@gmail.com>
2016-04-30 12:59:03 +0100

    Create barebones contact page.

---
+++ contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body>
+        <div id="fouc">
+            <div class="site-wrap">
+                <?php include("header.php"); ?>
+
+                <!-- Add page content here -->
+                <div class="page-content contact">
+                    <div class="contact-form">
+                        <h1>Contact</h1>
+
+                    </div>
+
+                    <div class="contact-image">
+                        <img src="/images/contact.jpg"/>
+                    </div>
+                </div>
+
+                <img class="back-to-top hidden" src="/images/icons/up-arrow.jpg"/>
+                <?php include("footer.php"); ?>
+            </div>
+        </div>
+
+        <?php include("javascript-files.php"); ?>
+    </body>
+</html>
