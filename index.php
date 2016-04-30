442ce0ca6a4700e4848b2544e093219914bcb326
Luke Twomey <luke.twomey85@gmail.com>
2016-04-30 12:47:31 +0100

    Add post dates to homepage previews. Add backup of blogger posts.

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
                         <a href="/blog/ho-chi-minh-city.php"><img class="featured-image" src="/images/ho-chi-minh.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/ho-chi-minh-city.php"><h2 class="post-title">Ho Chi Minh City</h2></a>
+                            <p class="post-date">4th July 2012 13:14</p>
                             <p>After the journey I'd been on to get here, Ho Chi Minh City certainly...</p>
                             <a href="/blog/ho-chi-minh-city.php" class="button">Read more...</a>
                         </div>
@@ -23,6 +24,7 @@
                         <a href="/blog/hanoi-to-saigon-part-two.php"><img class="featured-image" src="/images/hanoi-saigon-2.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/hanoi-to-saigon-part-two.php"><h2 class="post-title">Hanoi to Saigon - Part Two</h2></a>
+                            <p class="post-date">28th June 2012 10:26</p>
                             <p>Day five should have seen us arrive in Khe Sanh after a two hundred ki...</p>
                             <a href="/blog/hanoi-to-saigon-part-two.php" class="button">Read more...</a>
                         </div>
@@ -31,6 +33,7 @@
                         <a href="/blog/hanoi-to-saigon-part-one.php"><img class="featured-image" src="/images/hanoi-saigon.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/hanoi-to-saigon-part-one.php"><h2 class="post-title">Hanoi to Saigon - Part One</h2></a>
+                            <p class="post-date">15th June 2012 14:58</p>
                             <p>This was it, the big one. The Northwest Loop was just the practice run...</p>
                             <a href="/blog/hanoi-to-saigon-part-one.php" class="button">Read more...</a>
                         </div>
@@ -39,6 +42,7 @@
                         <a href="/blog/vietnam-by-motorbike.php"><img class="featured-image" src="/images/vietnam-by-bike.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/vietnam-by-motorbike.php"><h2 class="post-title">Vietnam by Motorbike</h2></a>
+                            <p class="post-date">26th May 2012 15:51</p>
                             <p>So I bought a motorbike. A "Honda Win" that isn't actually a Honda, bu...</p>
                             <a href="/blog/vietnam-by-motorbike.php" class="button">Read more...</a>
                         </div>
@@ -47,6 +51,7 @@
                         <a href="/blog/arrival-in-hanoi.php"><img class="featured-image" src="/images/hanoi.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/arrival-in-hanoi.php"><h2 class="post-title">Arrival in Hanoi</h2></a>
+                            <p class="post-date">14th May 2012 16:31</p>
                             <p>They played surreal, remixed, poor quality Vietnamese music videos on...</p>
                             <a href="/blog/arrival-in-hanoi.php" class="button">Read more...</a>
                         </div>
@@ -55,6 +60,7 @@
                         <a href="/blog/arrival-in-nanning.php"><img class="featured-image" src="/images/nanning.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/arrival-in-nanning.php"><h2 class="post-title">Arrival in Nanning</h2></a>
+                            <p class="post-date">12th May 2012 11:24</p>
                             <p>The first train journey on my trip so far was mercifully uneventful. I...</p>
                             <a href="/blog/arrival-in-nanning.php" class="button">Read more...</a>
                         </div>
@@ -63,6 +69,7 @@
                         <a href="/blog/arrival-in-guilin.php"><img class="featured-image" src="/images/guilin.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/arrival-in-guilin.php"><h2 class="post-title">Arrival in Guilin</h2></a>
+                            <p class="post-date">6th May 2012 09:50</p>
                             <p>The drivers here are INSANE. I'd only been in Guilin half an hour and...</p>
                             <a href="/blog/arrival-in-guilin.php" class="button">Read more...</a>
                         </div>
@@ -71,6 +78,7 @@
                         <a href="/blog/macau-and-farewell-to-hong-kong.php"><img class="featured-image" src="/images/macau.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/macau-and-farewell-to-hong-kong.php"><h2 class="post-title">Macau and Farewell to Hong Kong</h2></a>
+                            <p class="post-date">5th May 2012 18:50</p>
                             <p>I'd read that a trip to Hong Kong wasn't complete without a visit to M...</p>
                             <a href="/blog/macau-and-farewell-to-hong-kong.php" class="button">Read more...</a>
                         </div>
@@ -80,6 +88,7 @@
                         <a href="/blog/exploring-hong-kong.php"><img class="featured-image" src="/images/hong-kong-explore.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/exploring-hong-kong.php"><h2 class="post-title">Exploring Hong Kong</h2></a>
+                            <p class="post-date">25th April 2012 17:28</p>
                             <p>CouchSurfing. Hands up if you've heard of it! No prizes if I...</p>
                             <a href="/blog/exploring-hong-kong.php" class="button">Read more...</a>
                         </div>
@@ -89,6 +98,7 @@
                         <a href="/blog/arrival-in-hong-kong.php"><img class="featured-image" src="/images/hong-kong.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/arrival-in-hong-kong.php"><h2 class="post-title">Arrival in Hong Kong</h2></a>
+                            <p class="post-date">19th April 2012 06:04</p>
                             <p>Well, I made it! The flight wasn't the most fun in the world, I don't...</p>
                             <a href="/blog/arrival-in-hong-kong.php" class="button">Read more...</a>
                         </div>
@@ -98,6 +108,7 @@
                         <a href="/blog/the-itinerary.php"><img class="featured-image" src="/images/itinerary.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/the-itinerary.php"><h2 class="post-title">The Itinerary</h2></a>
+                            <p class="post-date">28th March 2012 15:51</p>
                             <p>Other than the order of the places I'm visiting, I'm trying to keep th...</p>
                             <a href="/blog/the-itinerary.php" class="button">Read more...</a>
                         </div>
@@ -107,6 +118,7 @@
                         <a href="/blog/hello-and-welcome.php"><img class="featured-image" src="/images/welcome.jpg"/></a>
                         <div class="preview-text">
                             <a href="/blog/hello-and-welcome.php"><h2 class="post-title">Hello and Welcome!</h2></a>
+                            <p class="post-date">27th March 2012 23:18</p>
                             <p>My name's Luke Twomey, and if you're reading this I've probably harass...</p>
                             <a href="/blog/hello-and-welcome.php" class="button">Read more...</a>
                         </div>
